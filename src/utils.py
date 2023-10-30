97a225f7dc1c1c8720a6e20baed82659cd3074e2
ryfe <denis.koptelev@gmail.com>
2023-10-30 17:55:17 +0300

    add docstring and config flake8

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,24 @@ load_info = os.path.join("../js/operations.json")
 
 
 def info_operation(filename: json) -> list:
+    """
+    Функция обрабатывает json файл.
+    :param filename: json file.
+    :return: Преобразованный в список json файл.
+    """
     with open(filename, "r", encoding="utf8") as file:
         info = json.load(file)
         return info
 
 
 def get_operations_executed(data: list) -> list:
+    """
+    Функция принимает список и фильтрует его по следующим категориям.
+    Статус операции == "EXECUTED".
+    В списке имеется "from"
+    :param data: Список с данными об операциях.
+    :return: Фильтрованный по правилу список данных
+    """
     operations_executed = []
     for operation in data:
         if "state" in operation and operation["state"] == "EXECUTED":
@@ -24,16 +36,29 @@ def get_operations_executed(data: list) -> list:
 
 
 def get_last_five_operations(operation_with_list: list) -> list:
-    sort_operations = sorted(operation_with_list, key=lambda operation: operation["date"], reverse=True)
+    """
+    Функция сортирует данные по дате, забирает из списка последние пять операций.
+    :param operation_with_list: Список с данными об оперциях.
+    :return: Список последних пяти операций.
+    """
+    sort_operations = sorted(
+        operation_with_list, key=lambda operation: operation["date"], reverse=True
+    )
     last_five_operations = sort_operations[0:5]
     return last_five_operations
 
 
-def get_operations_formatted(last_five_operations: list):
+def get_operations_formatted(last_five_operations: list) -> list:
+    """
+    Функция преобразует дату в удобный для чтения формат,
+    маскирует номер счета и карты.
+    :param last_five_operations: Список с данными об операциях.
+    :return: Измененные по правилам данные.
+    """
     operations_formatted_list = []
     for operation in last_five_operations:
-        date = datetime.strptime(operation["date"], "%Y-%m-%dT%H:%M:%S.%f").strftime(
-            "%d.%m.%Y")
+        date = datetime.strptime(operation["date"],
+                                 "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
         description = operation["description"]
         payer_info, payment_method = "", ""
         if "from" in operation:
@@ -42,11 +67,15 @@ def get_operations_formatted(last_five_operations: list):
             if payer[0].lower() == "счет":
                 payment_method_from = f"**{payment_method[-4:]}"
             else:
-                payment_method_from = f"{payment_method[:4]} {payment_method[4:6]}** **** {payment_method[12:]}"
+                payment_method_from = (f"{payment_method[:4]} "
+                                       f"{payment_method[4:6]}** **** {payment_method[12:]}")
             payer_info = " ".join(payer)
         recipients = f"{operation['to'].split()[0]} **{operation['to'][-4:]}"
-        operation_amount = f"{operation['operationAmount']['amount']} {operation['operationAmount']['currency']['name']}"
-        operations_formatted_list.append(f"""        {date} {description}
+        operation_amount = (f"{operation['operationAmount']['amount']} "
+                            f"{operation['operationAmount']['currency']['name']}")
+        operations_formatted_list.append(
+            f"""        {date} {description}
         {payer_info} {payment_method_from}->{recipients}
-        {operation_amount}""")
+        {operation_amount}"""
+        )
     return operations_formatted_list
